acef56f4472469b82f402d5c11aa6f34bc4753c8
ZuleykhaBakhshiyeva <bakhshiyevazuleykha@gmail.com>
2022-11-04 22:15:02 +0400

    9 10

--- index.js
+++ index.js
@@ -174,24 +174,24 @@ function getRandomNumber(min, max) {
 
 ////////////////////////////////////////////////////////////////
 //10. Sort the array in descending order in one of the ways: bubble sort, selection sort or insertion sort.
-const itemsCount10 = getNumberFromUser(`Please enter the number:`);
-const array10 = [];
-for (let index = 0; index < itemsCount10; index++) {
-    array9[index] = getRandomNumber(0, 100);
-    console.log(array10[index]);
-}
+//const itemsCount10 = getNumberFromUser(`Please enter the number:`);
+//const array10 = [];
+//for (let index = 0; index < itemsCount10; index++) {
+ //   array9[index] = getRandomNumber(0, 100);
+ //   console.log(array10[index]);
+//}
 
-for (let i = 0; i < array10.length - 2; i++){
-  large = i;
-  for(let j = i; j < array10.length; j++){
-      if(array10[j]> array10[large]){
-          large = j;
-      }
-  }
-  temp = array10[large];
-              array10[large] = array10[i];
-              array10[i] = temp;
-}
-console.log(array10) 
+//for (let i = 0; i < array10.length - 2; i++){
+ // large = i;
+  //for(let j = i; j < array10.length; j++){
+   //   if(array10[j]> array10[large]){
+     //     large = j;
+     // }
+ // }
+ // temp = array10[large];
+   //           array10[large] = array10[i];
+     //         array10[i] = temp;
+//}
+//console.log(array10) 
 
 
